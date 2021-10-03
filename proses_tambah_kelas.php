447b748d57b9cc2a53d8a06319086f2e3dce2015
NisaaAp <85125459+NisaaAp@users.noreply.github.com>
2021-10-03 22:31:46 +0700

    Create proses_tambah_kelas.php

---
+++ proses_tambah_kelas.php
@@ -0,0 +1,17 @@
+<?php
+    if($_POST){
+        $nama_kelas=$_POST['nama_kelas'];
+        $kelompok=$_POST['kelompok'];
+
+        include "koneksi.php";
+        $input=mysqli_query($koneksi, "INSERT INTO kelas (nama_kelas,
+        kelompok) value ('".$nama_kelas."','".$kelompok."')");
+        if($input){
+        echo "<script>alert('Berhasil');location.href='tambah_kelas.php';</script>";
+        } else {
+        echo "<script>alert('Gagal');location.href='tambah_kelas.php';</script>";
+        }
+        }
+  
+
+?>
